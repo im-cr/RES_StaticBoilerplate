f13836afbf605a8ce60c307d17e30a8448d25ecc
cortney robinson <gvestudios@gmail.com>
2014-09-18 20:51:15 -0400

    boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp');
+
+
+//plugins
+var sass = require('gulp-sass'),
+   cssmin = require('gulp-cssmin'),
+   changed = require('gulp-changed'),
+   concat = require('gulp-concat'),
+   watch = require('gulp-watch'),
+   plumber = require('gulp-plumber'),
+   prefix = require('gulp-autoprefixer'),
+   uglify = require('gulp-uglify'),
+   browserSync = require('browser-sync'),
+   connect = require('gulp-connect');
+   livereload = require('gulp-livereload');
+
+
+
+var paths = {
+   html    : "./*.html",
+   sass    : "./assets/css/sass/**/*.scss",
+   css     : "./assets/css/",
+   jssrc   : "./assets/js/src/*.js",
+   js      : "./assets/js/"
+}
+
+gulp.task('html',function(){
+  return gulp.src(paths.html)
+  .pipe(changed('./'))
+  .pipe(livereload());
+});
+
+// COMPILE SASS
+//-----------------------------------------------
+gulp.task('styles',function(){
+   return gulp.src(paths.sass)
+   .pipe(plumber())
+   .pipe(sass())
+   .pipe(prefix('last 2 versions'))
+   .pipe(cssmin())
+   .pipe(gulp.dest(paths.css))
+   .pipe(livereload());
+});
+
+
+//Minify JS SCRIPTS
+//----------------------------------------------
+gulp.task('scripts',function(){
+   return gulp.src(paths.jssrc)
+   .pipe(uglify())
+   .pipe(concat('scripts.js'))
+   .pipe(gulp.dest(paths.js));
+});
+
+//Watch the sass file changes
+//----------------------------------------------
+gulp.task('watch',function(){
+   var server = livereload();
+  // Gulp watches for changes in styles for triggering 'styles' function
+  gulp.watch(paths.sass, ['styles']);
+  gulp.watch(paths.jssrc,['scripts']);
+  gulp.watch(paths.html,['html']);
+});
+
+//CONNECT TO SERVER
+//-----------------------------------------------
+gulp.task('webserver',function(){
+   connect.server({
+      livereload : true
+   });
+});
+
+gulp.task('livereload',function(){
+   gulp.src([paths.sass, paths.jssrc,paths.html])
+   .pipe(watch())
+   .pipe(connect.reload());
+});
+
+gulp.task('default',['webserver','styles','scripts','livereload','watch']);
