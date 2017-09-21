5e3e4d711eda2879b6555bd31bb9d74af6085f46
Cortney Robinson <gvestudios@gmail.com>
2017-09-21 16:15:50 -0400

    Nunjucks update for boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var browserSync = require('browser-sync').create(),
     plumber = require('gulp-plumber'),
     prefix = require('gulp-autoprefixer'),
     merge = require('merge'),
+    nunjucksRender = require('gulp-nunjucks-render'),
     rename = require('gulp-rename'),
     replace = require('gulp-regex-replace'),
     sass = require('gulp-sass'),
@@ -29,12 +30,15 @@ var browserSync = require('browser-sync').create(),
 
 // ------------------------------- DIR/FILE PATHS
 var paths = {
-   html    : "./app/*.html",
-   sass    : "./app/assets/css/sass/**/*.scss",
-   css     : "./app/assets/css/",
-   jsv     : "./app/assets/js/vendor/*.js",
-   srcjs   : "./app/assets/js/src/*.js",
-   js      : "./app/assets/js/"
+    public   : "./app/__public/",
+    html     : "./app/*.html",
+    sass     : "./app/assets/css/sass/**/*.scss",
+    css      : "./app/assets/css/",
+    jsv      : "./app/assets/js/vendor/*.js",
+    srcjs    : "./app/assets/js/src/*.js",
+    js       : "./app/assets/js/",
+    pages    : "./app/pages/**/*.nunjucks",
+    jucks    : "./app/templates/**/*.nunjucks"
 }
 
 // ------------------------------- MARKUP TASKS
@@ -43,8 +47,8 @@ gulp.task('html',function(){
   .pipe(changed('./'));
 });
 
-// ------------------------------- CSS TASKS
-//compile sass
+//CSS TASKS
+//----------------------------------------------
 gulp.task('styles',function(){
    return gulp.src(paths.sass)
    .pipe(changed(paths.css))
@@ -52,12 +56,15 @@ gulp.task('styles',function(){
    .pipe(sass())
    .pipe(prefix('last 2 versions'))
    .pipe(cssmin())
+   .pipe(rename('style.public.css'))
+   .pipe(gulp.dest(paths.public))
    .pipe(rename('style.min.css'))
    .pipe(gulp.dest(paths.css));
 });
 
 
-// ------------------------------- JS TASKS
+//JS TASKS
+//----------------------------------------------
 gulp.task('scripts',function(){
   var myjs = gulp.src(paths.srcjs)
    .pipe(changed(paths.srcjs))
@@ -70,44 +77,60 @@ gulp.task('scripts',function(){
    .pipe(gulp.dest(paths.js));
 
    //merge streams
-   return merge(myjs,vendorjs);
+   return merge(myjs);
 });
 
-//Watch the sass file changes
+//NUNKJUCKS HTML TASK
+//----------------------------------------------
+gulp.task('nunjucks', function() {
+
+  // Gets .html and .nunjucks files in pages
+  return gulp.src(paths.pages)
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['./app/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('./app/__public'))
+});
+
+
+//WATCH TASKS
 //----------------------------------------------
 gulp.task('watch',function(){
-   var server = livereload();
+   var server = browserSync.reload;
   // Gulp watches for changes in styles for triggering 'styles' function
   gulp.watch(paths.sass, ['styles']);
   gulp.watch(paths.srcjs,['scripts']);
   gulp.watch(paths.html,['html']);
+  gulp.watch(paths.pages,['nunjucks']);
+  gulp.watch(paths.jucks,['nunjucks']);
 });
 
 //CONNECT TO SERVER
 //-----------------------------------------------
 gulp.task('webserver',function(){
-   /*connect.server({
-      root: 'app',
-      port: 9001,
-      livereload : true
-   });
-  */
 
   var reload = browserSync.reload;
+
   browserSync.init({
     port: 9001,
-    server: "./app",
-    open: false
-    //proxy: "yourlocal.dev"
+    open: false,
+    server: "./app/__public",
+    //browser: ["google chrome", "safari"]
   });
-  gulp.watch([paths.html,paths.sass,paths.srcjs]).on('change', reload);
-
+  gulp.watch([paths.html,paths.sass,paths.srcjs,paths.jucks,paths.pages]).on('change', reload);
 });
 
+//Live Reload
+//----------------------------------------------
 gulp.task('livereload',function(){
-   gulp.src([paths.sass,paths.srcjs,paths.html])
+   gulp.src([paths.sass,paths.srcjs,paths.html,paths.jucks,paths.pages])
    .pipe(watch())
    .pipe(connect.reload());
 });
 
-gulp.task('default',['webserver','styles','livereload','scripts','watch']);
+
+//DEFAULT TASK
+//----------------------------------------------
+gulp.task('default',['watch','webserver','styles','nunjucks','scripts']);
